0a1be3c8b12782648a8324ab0420b7a13feba5a3
KHATRIRAMESH <khatriramesh972@gmail.com>
2024-11-23 13:56:08 +0545

    outh completed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -3,9 +3,6 @@ import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
-
-
-
 //Signup Controller
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -35,43 +32,92 @@ export const signup = async (req, res, next) => {
   }
 };
 
-
-
 //Sign in Controller
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password || email === "" || password == null) {
-   return next(errorHandler(400, "All fields are required!"));
+    return next(errorHandler(400, "All fields are required!"));
   }
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) {
-     return next(errorHandler(404, "User not found!"));
+      return next(errorHandler(404, "User not found!"));
     }
 
     const validPassword = bcrypt.compareSync(password, validUser.password);
     if (!validPassword) {
-     return next(errorHandler(400, "Invalid credentials!"));
-    } 
+      return next(errorHandler(400, "Invalid credentials!"));
+    }
 
-    
     const token = jwt.sign(
       {
-        id: validUser._id
+        id: validUser._id,
       },
-      process.env.JWT_SECRET_KEY,
+      process.env.JWT_SECRET_KEY
     );
 
-    const {password:pass,...rest }=validUser._doc;
-    
-    res.status(200).cookie("access-token", token, {
-      httpOnly: true,
+    const { password: pass, ...rest } = validUser._doc;
 
-    }).json(rest)
-    console.log(rest)
+    res
+      .status(200)
+      .cookie("access-token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+    console.log(rest);
+  } catch (error) {
+    next(error);
+  }
+};
 
+//Signin with google
 
+export const google = async (req, res, next) => {
+  const { name, email, googlePhotoUrl } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+      const { password, ...rest } = user._doc;
+      res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .json(rest);
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+
+      const hashedPassword = await bcrypt.hash(generatedPassword, 10);
+      const newUser = new User({
+        username:
+          name.toLowerCase().split(" ").join("") +
+          Math.random().toString(9).slice(-4),
+        email,
+        password: hashedPassword,
+        profilePicture: googlePhotoUrl,
+      });
+
+      await newUser.save();
+
+      const token = jwt.sign(
+        {
+          id: newUser._id,
+        },
+        process.env.JWT_SECRET_KEY
+      );
+
+      const { password, ...rest } = newUser._doc;
+      res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .json(rest);
+    }
   } catch (error) {
     next(error);
   }
--- frontend/src/components/Oauth.jsx
+++ frontend/src/components/Oauth.jsx
@@ -2,16 +2,39 @@ import { Button } from "flowbite-react";
 import { AiFillGoogleCircle } from "react-icons/ai";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase.js";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const Oauth = () => {
+  const navigate = useNavigate();
+
   const auth = getAuth(app);
+
+  const dispatch = useDispatch();
 
   const handleGoogleClick = async () => {
     const provider = new GoogleAuthProvider();
     provider.setCustomParameters({ prompt: "select_account" });
     try {
       const resultsFromGoogle = await signInWithPopup(auth, provider);
-      console.log(resultsFromGoogle);
+
+      const res = await fetch("/api/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: resultsFromGoogle.user.displayName,
+          email: resultsFromGoogle.user.email,
+          googlePhotoUrl: resultsFromGoogle.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate("/");
+      }
     } catch (error) {
       console.log(error);
     }
