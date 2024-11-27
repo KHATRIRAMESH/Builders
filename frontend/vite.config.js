693d1882bb9909173e6b8a776c8829b3bbd8aa10
KHATRIRAMESH <khatriramesh972@gmail.com>
2024-11-27 19:37:52 +0545

    api configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://builders-xags.vercel.app",
         secure: false,
       },
     },
